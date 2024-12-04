40909529138f4732e3727f61447f52cd62e52e0f
Seth May <sethmay@gmail.com>
2024-12-03 16:39:47 -0800

    Day 3 part 2 - working

--- day03/03-2.py
+++ day03/03-2.py
@@ -2,36 +2,28 @@ import sys, re
 sys.path.append("../helpers.py")
 from helpers import *
 
-#input_data = load_input(3, InputType.MAIN)
-input_data = load_input(3, InputType.EXAMPLE)
+input_data = load_input(3, InputType.MAIN)
+#input_data = load_input(3, InputType.EXAMPLE).replace("\n", '')
 
 
 do_donts = input_data.split('do()')
-#print(do_donts)
 
-test = []
+do_calcs = []
 for item in do_donts:
-    match = re.search(r"don\'t\(\)", item)
-    print(match)
-    if (match):
-        test.append(item[match.end():])
+    pos = item.find("don't()")
+    if (pos != -1):
+        do_calcs.append(item[:item.find("don't()")])
+    else:
+        do_calcs.append(item)
 
-#print(do_donts)
+data = "".join(do_calcs)
+pattern = r"mul\([0-9]{1,3},[0-9]{1,3}\)"
+matches = re.findall(pattern, data)
 
-#print(test)
+mult_pattern = r"[0-9]{1,3}"
+total = 0
+for expression in matches:
+    items = re.findall(mult_pattern, expression)
+    total += int(items[0]) * int(items[1])
 
-# pattern = r"mul\([0-9]{1,3},[0-9]{1,3}\)"
-# matches = re.findall(pattern, input_data)
-#
-# mult_pattern = r"[0-9]{1,3}"
-# total = 0
-# for expression in matches:
-#     items = re.findall(mult_pattern, expression)
-#     total += int(items[0]) * int(items[1])
-#
-# print(total)
-
-
-
-#https://stackoverflow.com/questions/3475251/split-a-string-by-a-delimiter-in-python
-#https://stackoverflow.com/questions/46766530/python-split-a-string-by-the-position-of-a-character
+print(total)
